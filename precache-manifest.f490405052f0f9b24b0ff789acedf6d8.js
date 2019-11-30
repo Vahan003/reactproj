6fa5e4d6318dae09f31f9f58eac6569a0073b7f9
Your Name <you@example.com>
2019-11-30 16:32:31 +0400

    Updates

---
+++ precache-manifest.f490405052f0f9b24b0ff789acedf6d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf87dc89762703709ea891ab86261bc",
+    "url": "/reactproj/index.html"
+  },
+  {
+    "revision": "d345577398a61e1c805e",
+    "url": "/reactproj/static/css/main.cf3af14a.chunk.css"
+  },
+  {
+    "revision": "c8fc42cc54a6b13dfbf9",
+    "url": "/reactproj/static/js/2.b09e5893.chunk.js"
+  },
+  {
+    "revision": "d345577398a61e1c805e",
+    "url": "/reactproj/static/js/main.4070cc8e.chunk.js"
+  },
+  {
+    "revision": "35d59ae0016243b88e1b",
+    "url": "/reactproj/static/js/runtime-main.97ee5759.js"
+  },
+  {
+    "revision": "2eaab63570afef5efd2a77e995e4fccf",
+    "url": "/reactproj/static/media/home-b.2eaab635.png"
+  },
+  {
+    "revision": "33d92ee128af00a2685993590d38c90f",
+    "url": "/reactproj/static/media/login-b.33d92ee1.jpg"
+  },
+  {
+    "revision": "fd0a2e0e1dd2d0c5e123e70293feb464",
+    "url": "/reactproj/static/media/tabs-b.fd0a2e0e.jpg"
+  }
+]);
